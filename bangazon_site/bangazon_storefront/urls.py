88fc3d88601ee11c9b302ca8719734294b0b7ad6
Steven Holmes <steven.w.holmes@gmail.com>
2017-02-24 11:03:44 -0600

    PaymentType tests + model + most recent view

--- bangazon_site/bangazon_storefront/urls.py
+++ bangazon_site/bangazon_storefront/urls.py
@@ -1,12 +1,13 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-from bangazon_storefront.views import product_types_view, customer_view
+from bangazon_storefront.views import product_types_view, customer_view, paymenttype_view
 
 
 app_name = 'bangazon_storefront'
 urlpatterns = [
     url(r'^productTypes/', product_types_view.productTypes, name="productTypes"),
     url(r'^customer', customer_view.CustomerView.as_view(), name='CustomerView'),
+    url(r'^payment', paymenttype_view.PaymentTypeView.as_view(), name='PaymentTypeView'),
 ]
 

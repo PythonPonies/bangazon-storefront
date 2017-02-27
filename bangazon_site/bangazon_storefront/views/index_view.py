8de58b8f48c36f947a07375c63280f6ca9dd1a99
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-27 15:53:15 -0600

    logic added to navigation view

--- bangazon_site/bangazon_storefront/views/index_view.py
+++ bangazon_site/bangazon_storefront/views/index_view.py
@@ -1,21 +1,8 @@
 from django.views.generic.base import TemplateView
-from bangazon_storefront.models.order_model import *
-from bangazon_storefront.models.customer_model import *
+from bangazon_storefront.views.navigation_view import *
 from django.shortcuts import render
 
 def Index(request):
-    # check if user is logged index
-    if request.user.is_authenticated():
-        print(request.user, "is logged in")
-        # return number of their active products
-        # get customer
-        customer = Customer.objects.get(user=request.user)
-        order = Order.objects.get_or_create(buyer=customer, payment_complete=0)
-        order_number = order[0]
-        products = order_number.products.all().count()
-
-    else:
-        # if user is not logged in show 0 next to cart
-        products = 0
+    products = Navigation.get_products_for_active_customer(request)
     context = {'order_number': products}
     return render(request, 'bangazon_storefront/index.html', context)
--- bangazon_site/bangazon_storefront/views/navigation_view.py
+++ bangazon_site/bangazon_storefront/views/navigation_view.py
@@ -1,3 +1,6 @@
+from bangazon_storefront.models.order_model import *
+from bangazon_storefront.models.customer_model import *
+
 class Navigation():
     """
     The Navigation class contains methods needed by the navigation bar view.
@@ -7,9 +10,17 @@ class Navigation():
     Author:    Nate Baker, Main Bananas
     """
 
-    def get_products_for_active_customer():
-        print("get products runs!!!!")
+    def get_products_for_active_customer(request):
         """
         get_products_for_active_customer is a method that returns the number of products that the active customer currently has.
         """
-        return 2
+        if request.user.is_authenticated():
+            # return number of their active products
+            customer = Customer.objects.get(user=request.user)
+            order = Order.objects.get_or_create(buyer=customer, payment_complete=0)
+            order_number = order[0]
+            products = order_number.products.all().count()
+            return products
+        else:
+            # if user is not logged in show 0 next to cart
+            return 0

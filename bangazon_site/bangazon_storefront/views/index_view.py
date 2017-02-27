461dd05a8375bf0275673e955d0d2c0f121bcd84
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-27 15:10:00 -0600

    navigation shows order in navigation

--- bangazon_site/bangazon_storefront/views/index_view.py
+++ bangazon_site/bangazon_storefront/views/index_view.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from bangazon_storefront.models.order_model import *
+from bangazon_storefront.models.customer_model import *
 from django.shortcuts import render
 
 def Index(request):
@@ -7,9 +8,14 @@ def Index(request):
     if request.user.is_authenticated():
         print(request.user, "is logged in")
         # return number of their active products
-        order_number = 33
+        # get customer
+        customer = Customer.objects.get(user=request.user)
+        order = Order.objects.get_or_create(buyer=customer, payment_complete=0)
+        order_number = order[0]
+        products = order_number.products.all().count()
+
     else:
         # if user is not logged in show 0 next to cart
-        order_number = 0
-    context = {'order_number': order_number}
+        products = 0
+    context = {'order_number': products}
     return render(request, 'bangazon_storefront/index.html', context)

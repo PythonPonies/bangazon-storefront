09b1e3870faac5abd5c42441f14303f8acc2160e
Ikechi <consumerike@gmail.com>
2017-02-28 15:37:36 -0600

    committing before PR check

--- bangazon_site/bangazon_storefront/views/all_products_view.py
+++ bangazon_site/bangazon_storefront/views/all_products_view.py
@@ -7,6 +7,7 @@ from bangazon_storefront.models.products_model import *
 
 def all_products(request, productTypes_id):
     all_products = ProductsModel.objects.filter(categoryId=productTypes_id)
+    product_type_name = ProductTypes.objects.get(categoryId=productTypes_id)
     print(all_products, "these are products in the all_products_view line 10")
     context =  {'products' : all_products, "productTypes_id": productTypes_id} 
     return render(request, 'bangazon_storefront/all_products.html', context)

0393ed626e684af043b53f4009778948b04f15d9
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-02-28 16:40:26 -0600

    got order view

--- bangazon_site/bangazon_storefront/views/addproducttoorder_view.py
+++ bangazon_site/bangazon_storefront/views/addproducttoorder_view.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 def add_product_to_order(request):
     """This method gets data from the product detail page about the product, gets the customer order, and adds product to the order. Then it redirects to products type page"""
     data = request.POST
+    print(data['product_id'])
     product = products_model.ProductsModel.objects.get(pk=data['product_id'])
     customer = customer_model.Customer.objects.get(user=request.user)
     order = order_model.Order.objects.get_or_create(buyer = customer, payment_complete=0)
